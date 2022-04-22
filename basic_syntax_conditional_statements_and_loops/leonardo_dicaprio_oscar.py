b291cdc15c83b722985cbba11bfae7ba31527710
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-22 20:47:56 +0300

    Create leonardo_dicaprio_oscar.py

---
+++ basic_syntax_conditional_statements_and_loops/leonardo_dicaprio_oscar.py
@@ -0,0 +1,13 @@
+number = int(input())
+
+if number <= 88:
+    if number == 88:
+        print("Leo finally won the Oscar! Leo is happy")
+    elif number == 86:
+        print("Not even for Wolf of Wall Street?!")
+    else:
+        print("When will you give Leo an Oscar?")
+
+else:
+    print("Leo got one already!")
+    
