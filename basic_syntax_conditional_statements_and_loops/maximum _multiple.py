de7e2529d271dd6b6f3d69034bf9e53ebcb6217e
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-23 23:14:54 +0300

    Update maximum _multiple.py

--- basic_syntax_conditional_statements_and_loops/maximum _multiple.py
+++ basic_syntax_conditional_statements_and_loops/maximum _multiple.py
@@ -1,19 +1,10 @@
-budget = float(input())
-flour_price_per_kg = float(input())
-breads_counter = 0
-eggs_counter = 0
+divisor = int(input())
+bound = int(input())
+max_multiply = 0
 
-while True:
-    price_per_bread = flour_price_per_kg + (flour_price_per_kg * 0.75) + (flour_price_per_kg * 1.25 / 4)
-    if price_per_bread > budget:
-        break
-    budget -= price_per_bread
-    breads_counter += 1
-    eggs_counter += 3
-    if breads_counter % 3 == 0:
-        eggs_counter -= breads_counter - 2
-
-
-print(f"You made {breads_counter} loaves of Easter bread! Now you have {eggs_counter} eggs and {budget:.2f}BGN left.")
+for number in range(divisor + 1, bound + 1):
+    if number % divisor == 0:
+        max_multiply = number
+print(max_multiply)
 
 
