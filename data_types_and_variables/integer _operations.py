a4b524ad61e454062d8e1b71eeec642e3dc8f812
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-18 13:57:57 +0300

    Add files via upload

---
+++ data_types_and_variables/integer _operations.py
@@ -0,0 +1,8 @@
+number_one = int(input())
+number_two = int(input())
+number_tree = int(input())
+number_four = int(input())
+
+result = int((number_one + number_two) / number_tree) * number_four
+
+print(result)
