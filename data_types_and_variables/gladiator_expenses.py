1d31ee1aed802f558c2ba51e6ea7eb05745467e4
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-18 13:57:16 +0300

    Add files via upload

---
+++ data_types_and_variables/gladiator_expenses.py
@@ -0,0 +1,21 @@
+lost_fights_count = int(input())
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+
+expences = 0
+for fights in range (1, lost_fights_count + 1):
+    if fights % 2 == 0:
+        expences += helmet_price
+    if fights % 3 == 0:
+        expences += sword_price
+    if fights % 2 == 0 and fights % 3 == 0:
+        expences += shield_price
+    if fights % 12 == 0:
+        expences += armor_price
+
+print(f"Gladiator expenses: {expences:.2f} aureus")
+
+
+
