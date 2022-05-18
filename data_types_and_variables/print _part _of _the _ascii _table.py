a991415bc420a87f63a15478b7ad4b3301126521
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-18 14:05:17 +0300

    Add files via upload

---
+++ data_types_and_variables/print _part _of _the _ascii _table.py
@@ -0,0 +1,11 @@
+first_index = int(input())
+second_index = int(input())
+
+for indexes in range(first_index, second_index + 1):
+    current_index = chr(indexes)
+
+    print(current_index, end=" ")
+
+
+
+
