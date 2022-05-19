5d72ce86babc5d11f6f8a11b39d6d9f3586b728d
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-19 16:59:10 +0300

    Delete triples _of _latin _letters.py

--- triples _of _latin _letters.py
+++
@@ -1,8 +0,0 @@
-number = int(input())
-for num_one in range(number):
-    char_one = chr(num_one + 97)
-    for num_two in range(number):
-        char_two = chr(num_two + 97)
-        for num_tree in range(number):
-            char_tree = chr(num_tree + 97)
-            print(char_one + char_two + char_tree)
