b85b03c096af3e0d498e0e5733ba9c67aa463a59
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-23 22:31:13 +0300

    Add files via upload

---
+++ basic_syntax_conditional_statements_and_loops/more_problems/how_much_coffee_do _you_need.py
@@ -0,0 +1,19 @@
+command = input()
+counter = 0
+
+while command != "END":
+    if command == "coding" or command == "cat" or command == "dog" or command == "movie":
+        counter += 1
+    elif command == "CODING" or command == "CAT" or command == "DOG" or command == "MOVIE":
+        counter += 2
+
+    if counter > 5:
+        print("You need extra sleep")
+        break
+
+    command = input()
+
+if counter <= 5:
+    print(counter)
+
+
