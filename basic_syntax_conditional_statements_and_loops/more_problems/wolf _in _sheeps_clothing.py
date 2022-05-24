62f7eee2f741963273874659b86ae571a147bacc
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-24 20:39:25 +0300

    Update wolf _in _sheeps_clothing.py

--- basic_syntax_conditional_statements_and_loops/more_problems/wolf _in _sheeps_clothing.py
+++ basic_syntax_conditional_statements_and_loops/more_problems/wolf _in _sheeps_clothing.py
@@ -7,6 +7,3 @@ if "wolf" in animals:
         index_wolf = animals.index("wolf")
         count_sheep = abs(index_wolf+1 - (len(animals)))
         print(f"Oi! Sheep number {count_sheep}! You are about to be eaten by a wolf!")
-
-
-
