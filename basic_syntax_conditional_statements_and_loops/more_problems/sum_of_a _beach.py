dceabdd2669362313db7a4ac9657e63de49cd22b
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-24 20:38:33 +0300

    Add files via upload

---
+++ basic_syntax_conditional_statements_and_loops/more_problems/sum_of_a _beach.py
@@ -0,0 +1,19 @@
+str_word = input().lower()
+counter = 0
+
+if "water" in str_word:
+    result = str_word.count("water")
+    counter += result
+if "sand" in str_word:
+    result = str_word.count("sand")
+    counter += result
+if "fish" in str_word:
+    result = str_word.count("fish")
+    counter += result
+if "sun" in str_word:
+    result = str_word.count("sun")
+    counter += result
+
+print(counter)
+
+
