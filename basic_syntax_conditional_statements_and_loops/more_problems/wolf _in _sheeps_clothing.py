d4cf5f4170ddf7d35dffa1ee823ba0eddbf72f1b
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-24 20:38:56 +0300

    Add files via upload

---
+++ basic_syntax_conditional_statements_and_loops/more_problems/wolf _in _sheeps_clothing.py
@@ -0,0 +1,12 @@
+animals = input().split(", ")
+
+if "wolf" in animals:
+    if "wolf" == animals[-1]:
+        print("Please go away and stop eating my sheep")
+    else:
+        index_wolf = animals.index("wolf")
+        count_sheep = abs(index_wolf+1 - (len(animals)))
+        print(f"Oi! Sheep number {count_sheep}! You are about to be eaten by a wolf!")
+
+
+
