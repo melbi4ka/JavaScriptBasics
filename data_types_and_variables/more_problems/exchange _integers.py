ba7aaf123a45a340e9fbfd2695670358d74fe073
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-25 17:38:59 +0300

    Add files via upload

---
+++ data_types_and_variables/more_problems/exchange _integers.py
@@ -0,0 +1,15 @@
+first_num = int(input())
+second_num = int(input())
+
+a = first_num
+b = second_num
+c = second_num
+d = first_num
+
+print("Before:")
+print(f"a = {a}")
+print(f"b = {b}")
+print("After:")
+print(f"a = {c}")
+print(f"b = {d}")
+
