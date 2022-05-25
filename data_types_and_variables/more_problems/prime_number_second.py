cf5c70fea35d8f881c5313bcf69bf64931a2c2eb
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-25 17:39:59 +0300

    Add files via upload

---
+++ data_types_and_variables/more_problems/prime_number_second.py
@@ -0,0 +1,13 @@
+num = int(input())
+is_prime = True
+
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            is_prime = False
+            break
+
+if is_prime:
+    print("True")
+else:
+    print("False")
