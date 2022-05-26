ad5b0981afaf5ab056fb4872cbdae6d75851bbf8
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-26 19:46:54 +0300

    Add files via upload

---
+++ list_basic/faro _shuffle.py
@@ -0,0 +1,24 @@
+cards = input().split(" ")
+shuffles = int(input())
+
+new_cards_list = []
+
+for shuffles in range(shuffles):
+    new_cards_list = []
+    middle_cards = len(cards) // 2
+    left_side = cards[0:middle_cards]
+    right_side = cards[middle_cards::]
+    for index in range(len(left_side)):
+        new_cards_list.append(left_side[index])
+        new_cards_list.append(right_side[index])
+    cards = new_cards_list
+print(cards)
+
+
+
+
+
+
+
+
+
