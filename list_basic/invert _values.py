b3e72587efa88686933531d3db3bd4112339879b
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-27 15:30:48 +0300

    Add files via upload

---
+++ list_basic/invert _values.py
@@ -0,0 +1,11 @@
+start_list = input().split(" ")
+
+final_list = []
+for element in range(len(start_list)):
+    current_element = -int(start_list[element])
+    final_list.append(current_element)
+
+print(final_list)
+
+
+
