bbd1f09794876d75b23af8bfb4d1a2db5423be1e
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-29 22:18:56 +0300

    Add files via upload

---
+++ list_basic/survival _of _the_biggest.py
@@ -0,0 +1,35 @@
+import sys
+
+start_list = input().split()
+start_number = int(input())
+new_list = []
+
+for num in start_list:
+    new_list.append(int(num))
+
+for numbers in range(start_number):
+    removed_element = sys.maxsize
+
+    for element in new_list:
+        if element < removed_element:
+            removed_element = element
+
+    new_list.remove(removed_element)
+
+result = []
+for num in new_list:
+    result.append(str(num))
+print(", ".join(result))
+
+
+
+
+
+
+
+
+
+
+
+
+
