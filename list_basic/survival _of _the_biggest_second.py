24373678ee963052e867e4fb399ea2313dc1b732
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-30 21:06:44 +0300

    Add files via upload

---
+++ list_basic/survival _of _the_biggest_second.py
@@ -0,0 +1,16 @@
+numbers_str = input().split()
+remover = int(input())
+numbers = []
+
+for num in numbers_str:
+    numbers.append(int(num))
+
+for _ in range(remover):
+    numbers.remove(min(numbers))
+
+final_numbers = []
+for num in numbers:
+    final_numbers.append(str(num))
+print(", ".join(final_numbers))
+
+
