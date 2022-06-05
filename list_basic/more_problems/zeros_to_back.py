6eea141479b3251575d5bb40d42334d10b819266
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-05 20:01:01 +0300

    Add files via upload

---
+++ list_basic/more_problems/zeros_to_back.py
@@ -0,0 +1,16 @@
+my_list = input().split(", ")
+
+for element in my_list:
+    if element == "0":
+        my_list.remove(element)
+        my_list.append(element[-1])
+
+new_list = []
+for num in my_list:
+    new_list.append(int(num))
+
+print(new_list)
+
+
+
+
