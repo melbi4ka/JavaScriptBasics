a62689b35693ccfbe282af833bc0322ae60a2ba1
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-06 21:57:32 +0300

    Add files via upload

---
+++ list_basic/more_problems/zeros_to_back_second.py
@@ -0,0 +1,13 @@
+info = input().split(", ")
+
+temp = []
+zeros = []
+
+for zero in info:
+    if int(zero) == 0:
+        zeros.append(int(zero))
+    else:
+        temp.append(int(zero))
+print(temp + zeros)
+
+
