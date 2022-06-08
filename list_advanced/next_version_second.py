7babdc32d6e10a59998d1905a3bc4eee1e495a4a
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-08 21:39:51 +0300

    Update and rename next_version_shpv.py to next_version_second.py

--- list_advanced/next_version_second.py
+++ list_advanced/next_version_second.py
@@ -3,8 +3,6 @@ version[-1] += 1
 for index in range(len(version) - 1, -1, -1):
     if version[index] > 9:
         version[index] = 0
-        if index - 1 >= 0: # проверява дали следващия индекс напред не извън индексите, когато е наобратно последния тр да е 0
+        if index - 1 >= 0: 
             version[index - 1] += 1
 print(".".join([str(s) for s in version]))
-
-
