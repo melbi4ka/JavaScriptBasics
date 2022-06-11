bcf13196346887ac17f7d7d9b806d77050d38345
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-11 12:43:44 +0300

    Update еlectron_distribution.py

--- list_advanced/еlectron_distribution.py
+++ list_advanced/еlectron_distribution.py
@@ -3,18 +3,12 @@ position = 0
 electron_list = []
 
 while number > 0:
-    position += 1 # така позицията ни започва от 1
-    shell = 2 * (position ** 2) #формулата за пълнене на шела
+    position += 1 # position starts from 1
+    shell = 2 * (position ** 2) #shell filling formula
     if number > shell:
         electron_list.append(shell)
     else:
-        electron_list.append(number) # ако позицията дава възможност за повече, но от номера няма такъв остатък
+        electron_list.append(number) # if there is no remainder of the number, but the position allows for more
     number -= shell
 
 print(electron_list)
-
-
-
-
-
-
