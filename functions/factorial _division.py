3344fafcc0a5ee75cfe901df42d6754dd88ba28a
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-12 21:17:57 +0300

    Add files via upload

---
+++ functions/factorial _division.py
@@ -0,0 +1,16 @@
+from math import factorial
+
+
+def factorial_division(num_one, num_two):
+    factorial_one = factorial(num_one)
+    factorial_two = factorial(num_two)
+    division = factorial_one/factorial_two
+    return f"{division:.2f}"
+
+
+number_one = int(input())
+number_two = int(input())
+print(factorial_division(number_one, number_two))
+
+
+
