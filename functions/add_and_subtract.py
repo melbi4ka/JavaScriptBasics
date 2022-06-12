108080c2f787f870854a6054c3e37e830b5207cd
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-12 21:14:28 +0300

    Create add_and_subtract.py

---
+++ functions/add_and_subtract.py
@@ -0,0 +1,12 @@
+def sum_numbers(a, b):
+    return a + b
+
+
+def subtract(a, b, c):
+    return sum_numbers(a, b) - c
+
+
+num_one = int(input())
+num_two = int(input())
+num_three = int(input())
+print(subtract(num_one, num_two, num_three))
