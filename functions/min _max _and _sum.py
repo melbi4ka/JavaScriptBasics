c5a9bf8173851eee27d959bd940966ccd552a6f9
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-14 10:13:06 +0300

    Update min _max _and _sum.py

--- functions/min _max _and _sum.py
+++ functions/min _max _and _sum.py
@@ -5,11 +5,8 @@ def min_max_sum_function(nums):
     return min_num, max_num, sum_num
 
 
-
-
 numbers = list(map(int, input().split()))
 minumal_num, maximal_num, sum_numbers = min_max_sum_function(numbers)
 print(f"The minimum number is {minumal_num}")
 print(f"The maximum number is {maximal_num}")
 print(f"The sum number is: {sum_numbers}")
-
