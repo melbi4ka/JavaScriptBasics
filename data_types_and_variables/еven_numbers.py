dc332056bd6b9f7e5650b16a993af7e9b8c177a9
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-17 14:20:45 +0300

    Add files via upload

---
+++ data_types_and_variables/еven_numbers.py
@@ -0,0 +1,16 @@
+def check_even(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+
+number = input().split()
+new_number = map(int, number)
+even_numbers = filter(check_even, new_number)
+print(list(even_numbers))
+
+
+
+
+
+
