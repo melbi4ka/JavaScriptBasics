0ae6a308de3ed11064b3080426306bbdbb6579c8
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-20 12:23:21 +0300

    Add files via upload

---
+++ objects_and_classes/town_my_second.py
@@ -0,0 +1,21 @@
+class Town:
+    latitude = ""
+    longitude = ""
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def set_latitude(self, latitude):
+        Town.latitude = "".join(latitude)
+
+    def set_longitude(self, longitude):
+        Town.longitude = "".join(longitude)
+
+    def __repr__(self):
+        return f"Town: {self.name} | Latitude: {Town.latitude} | Longitude: {Town.longitude}"
+
+
+town = Town("Sofia")
+town.set_latitude("42° 41\' 51.04\" N")
+town.set_longitude("23° 19\' 26.94\" E")
+print(town)
