5ac63c225a7689f670c6a89b2761e75832860e26
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-21 22:45:16 +0300

    Create miner_task.py

---
+++ dictionaries/miner_task.py
@@ -0,0 +1,16 @@
+command = input()
+
+resource_dict = {}
+
+while command != "stop":
+    key = command
+    command_two = int(input())
+
+    if key not in resource_dict:
+        resource_dict[key] = 0
+    resource_dict[key] += command_two
+
+    command = input()
+
+for key, value in resource_dict.items():
+    print(f"{key} -> {value}")
