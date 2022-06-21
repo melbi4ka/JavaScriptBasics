69e225b6ce89905165656d5b5050f535570059d7
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-21 22:47:53 +0300

    Update capitals.py

--- dictionaries/capitals.py
+++ dictionaries/capitals.py
@@ -3,7 +3,3 @@ capitals = input().split(", ")
 countries_dict = dict(zip(countries, capitals))
 
 countries_for_print = {print(f"{country} -> {capital}") for country, capital in countries_dict.items()}
-
-
-
-
