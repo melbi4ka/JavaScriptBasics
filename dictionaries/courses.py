7ab306fb87ff038b905faab3da33d4ee390ad8ef
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-22 20:27:22 +0300

    Update courses.py

--- dictionaries/courses.py
+++ dictionaries/courses.py
@@ -17,20 +17,3 @@ for course_name, student in student_dict.items():
     print(f"{course_name}: {len(student)}")
     result = '\n-- '.join(student)
     print(f"-- {result}")
-
-# sorted_courses = sorted(student_dict.items(), key = lambda kvpt: -len(kvpt[1]))
-# for course_name, student in sorted_courses:
-#     print(f"{course_name}: {len(student)}")
-#     for name in sorted(student):
-#         print(f"-- {name}")
-#  сортиране първо по големината на курсовете и после вътре по азбучен ред 09/21 упражнения
-# When you receive the command "end", print the courses with their names
-# and total registered users, ordered by the number of registered users in descending order.
-# For each course print, the registered users ordered by name in ascending order (alphabetically).
-
-
-
-
-
-
-#print(student_dict)
