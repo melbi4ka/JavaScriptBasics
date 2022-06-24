e6adaf9d73d8b770111aefc5ca091f25de3cfd16
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-24 20:02:06 +0300

    Update student _academy.py

--- dictionaries/student _academy.py
+++ dictionaries/student _academy.py
@@ -13,17 +13,4 @@ for name, grade in student_dict.items():
     result = sum(grade) / len(grade)
     if result >= 4.50:
         print(f"{name} -> {result:.2f}")
-
-#print(student_dict)
-# можем да филтрираме и да ги напълним в нов речник,над търсената оценка
-# после да печатаме само филтрирания речник  09/21 упражнения
-# Order the
-# filtered students by average grade in descending order.
-# sorted_students = sorted(student_dict.items(), key = lambda kvpt: -kvpt[1])
-# for name, grades in sorted_students:
-#     print(f"{name} -> {grades:.2f}")
-
-
-
-
-
+        
