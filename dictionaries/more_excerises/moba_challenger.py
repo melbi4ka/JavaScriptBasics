b9232bf62a44feffdea9d4090dce44731e0b34f0
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-27 18:44:23 +0300

    Update moba_challenger.py

--- dictionaries/more_excerises/moba_challenger.py
+++ dictionaries/more_excerises/moba_challenger.py
@@ -28,33 +28,3 @@ players = players_dict
 
 for n in players_dict.values():
     print(sum(n.values()))
-
-
-
-# for key, val in players_dict.items():
-#     print(key)
-#     print(val.items())
-#     # sorted_val = sorted(val.items(), key = lambda kvpt: sum(kvpt[1]))
-#     # print(sorted_val)
-#     # print(sum(val.values()))
-
-
-
-
-
-
-
-# for key, value in players_dict.items():
-#     sum = 0
-#     for val in value.values():
-#         sum += val
-#     print(f"{key}: {sum} skill")
-#     for n in value:
-#         print(f"- {n} <::> {value[n]}")
-#
-# print(players_dict.items())
-# sorted_players = sorted(players_dict.items(), key = lambda kvpt: kvpt[1])
-# print(sorted_players)
-
-
-# {'Peter': {'Adc': 400, 'all': ...}, 'George': {'Jungle': 300, 'all': ...}, 'Simon': {'Mid': 200, 'Support': 250, 'all': .. }}
