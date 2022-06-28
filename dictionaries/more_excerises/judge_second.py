1083aa6769100740a8960192e68dcb343287459f
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-28 21:51:38 +0300

    Add files via upload

--- dictionaries/more_excerises/judge_second.py
+++ dictionaries/more_excerises/judge_second.py
@@ -8,23 +8,21 @@ while a_command != "no more time":
 
     if contest not in contest_dict:
         contest_dict[contest] = {}
+
+    if user not in contest_dict[contest]:
         contest_dict[contest][user] = points
-    else:
-        if user not in contest_dict:
+    elif user in contest_dict[contest]:
+        if contest_dict[contest][user] <= points:
             contest_dict[contest][user] = points
-        else:
-            if contest_dict[contest][user] < points:
-                contest_dict[contest][user] = points
 
     if user not in total_points:
         total_points[user] = {}
+
+    if contest not in total_points[user]:
         total_points[user][contest] = points
-    else:
-        if contest not in total_points[user]:
+    elif contest in total_points[user]:
+        if total_points[user][contest] <= points:
             total_points[user][contest] = points
-        else:
-            if total_points[user][contest] < points:
-                total_points[user][contest] = points
 
     a_command = input()
 
@@ -35,10 +33,10 @@ for key, points in total_points.items():
     totals = sum(points.values())
     totals_for_sorted[key] = totals
 
-
+# print(totals_for_sorted)
 for key, contestant in contest_dict.items():
     print(f"{key}: {len(contestant)} participants")
-    sorted_contestant = sorted(contestant.items(), key = lambda kvpt: -kvpt[1])
+    sorted_contestant = sorted(contestant.items(), key=lambda kvpt: (-kvpt[1],kvpt[0]))
     m = 0
     for val in sorted_contestant:
         m += 1
@@ -46,7 +44,9 @@ for key, contestant in contest_dict.items():
 
 print("Individual standings:")
 n = 0
-sorted_total_points = sorted(totals_for_sorted.items(), key = lambda kvpt: -kvpt[1])
+sorted_total_points = sorted(totals_for_sorted.items(), key=lambda kvpt: (-kvpt[1], kvpt[0]))
 for name, points in sorted_total_points:
     n += 1
     print(f"{n}. {name} -> {points}")
+
+# 100/100
