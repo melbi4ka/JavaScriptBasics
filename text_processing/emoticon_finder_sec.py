b18fca09ed4bc3eb067a66814306a0d375476bb7
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-05 20:21:56 +0300

    Update and rename emoticon_finder.py to emoticon_finder_sec.py

--- text_processing/emoticon_finder_sec.py
+++ text_processing/emoticon_finder_sec.py
@@ -1,9 +1,3 @@
-# line = input()
-#
-# for char in range(len(line)):
-#     if line[char] == ":" and ;ine[char+1] != " ":
-#         emoticon = line[char] + line[char + 1]
-#         print(emoticon)
 def find_emoticon(a_line):
     for el in range(len(a_line)):
         if a_line[el] == ":" and a_line[el + 1] != " ":
