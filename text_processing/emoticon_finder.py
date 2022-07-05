9874fa12735477501959c4464f76e26e64efdaa1
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-05 20:19:14 +0300

    Delete emoticon_finder.py

--- text_processing/emoticon_finder.py
+++
@@ -1,8 +0,0 @@
-def find_emoticon(a_line):
-    for el in range(len(a_line)):
-        if a_line[el] == ":" and a_line[el + 1] != " ":
-            emoticon = a_line[el] + a_line[el + 1]
-            print(emoticon)
-
-line = input()
-find_emoticon(line)
