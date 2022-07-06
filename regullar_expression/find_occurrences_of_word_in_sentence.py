a52f25f2712870eafe5bb7cbca77271a4c13d0ac
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-06 17:37:07 +0300

    Update find_occurrences_of_word_in_sentence.py

--- regullar_expression/find_occurrences_of_word_in_sentence.py
+++ regullar_expression/find_occurrences_of_word_in_sentence.py
@@ -1,19 +1,9 @@
-# import re
-#
-#
-# text = input()
-# word = input()
-# pattern = fr"\b{word}\b"
-#
-#
-# matches = re.findall(pattern, text, flags=re.I)
-# print(len(matches))
-
 import re
 
-text = input().lower()
-word = input().lower()
+text = input()
+word = input()
 pattern = fr"\b{word}\b"
 
-matches = re.findall(pattern, text)
+
+matches = re.findall(pattern, text, flags=re.I)
 print(len(matches))
