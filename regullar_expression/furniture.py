c19f3f692de647fd7e68c4cbe02396b0c22037db
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-07 21:09:44 +0300

    Update furniture.py

--- regullar_expression/furniture.py
+++ regullar_expression/furniture.py
@@ -3,7 +3,7 @@ import re
 line = input()
 
 pattern = r"^>>([A-Za-z]+)<<(\d+\.?\d+)!(\d+)$"
-# букви в низ, цифри в низ, точка 0 или 1 пъти(може да  я има, може да я няма), цифри в низ, цифри в низ за количество
+
 furniture = []
 total_price = 0
 
@@ -19,7 +19,3 @@ print("Bought furniture:")
 if len(furniture) > 0:
     print(*furniture, sep="\n")
 print(f"Total money spend: {total_price:.2f}")
-
-# pattern = r"^>>([a-zA-Z]+)<<([0-9]+[\.0-9]*)!([0-9]+)$"
-# pattern = r"^>>([A-Za-z]+)<<(\d+[\.\d]*)!(\d+)$"
-#  тези патърни не са много точни, тъй като мачват и цена като 0.56.56, което не е валидна цена
