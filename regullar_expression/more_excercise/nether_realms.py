8f4192fc73d6b09fe56a10f1ed8a708ba0c970fc
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-08 19:57:15 +0300

    Add files via upload

---
+++ regullar_expression/more_excercise/nether_realms.py
@@ -0,0 +1,49 @@
+import re
+
+line = input().split(",")
+
+pattern = r"[^0-9\-\+\*\/\.]"
+pattern_two = r"\-*((0|[0-9]+)[\.0-9]*)"
+
+
+demons_dict = {}
+
+for el in line:
+    el = el.strip()
+    demons_dict[el] = []
+    matches = re.findall(pattern, el)
+
+    health = 0
+    for n in matches:
+        health += ord(n)
+    demons_dict[el].append(health)
+
+    numbers = re.finditer(pattern_two, el)
+    damage = 0
+    for num in numbers:
+        damage += float(num.group())
+
+    for char in el:
+        if char == "*":
+            damage *= 2
+        elif char == "/":
+            damage /= 2
+
+    demons_dict[el].append(damage)
+
+sorted_demons_dict = sorted(demons_dict.items(), key=lambda kvpt: kvpt)
+
+for n in sorted_demons_dict:
+    print(f"{n[0]} - {n[1][0]} health, {n[1][1]:.2f} damage")
+
+
+print(sorted_demons_dict)
+
+# мачва всички чаръктъри без (0-9), arithmetic symbols ('+', '-', '*', '/', '.')
+# с това създавам хелта
+
+# мачва тире - може и да го има, може и да го няма
+# мачва група - 0 или 0-9 в низ, след това мачва 0-9 и точка нула и един пъти
+# всичко това като се разпечата в цяла група се мачва с - отпред
+# това създава демиджа
+
