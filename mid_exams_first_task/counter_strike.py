957da70a462ad5c085a33f820231f7fb20617f31
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-16 17:12:24 +0300

    Add files via upload

---
+++ mid_exams_first_task/counter_strike.py
@@ -0,0 +1,29 @@
+initial_energy = int(input())
+command = input()
+count_battle = 0
+
+
+while command != "End of battle":
+    current_command = int(command)
+    if current_command > initial_energy:
+        print(f"Not enough energy! Game ends with {count_battle} won battles and {initial_energy} energy")
+        break
+    else:
+        initial_energy -= current_command
+        count_battle += 1
+    if count_battle % 3 == 0:
+        initial_energy += count_battle
+
+    command = input()
+
+if command == "End of battle":
+    print(f"Won battles: {count_battle}. Energy left: {initial_energy}")
+
+
+
+
+
+
+
+
+
