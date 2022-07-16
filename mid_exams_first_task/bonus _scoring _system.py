4cc2a5f1f70fe753a004f4df7e756a014f256dfa
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-16 17:11:26 +0300

    Add files via upload

---
+++ mid_exams_first_task/bonus _scoring _system.py
@@ -0,0 +1,27 @@
+from math import ceil
+
+number_students = int(input())
+number_lectures = int(input())
+additional_bonus = int(input())
+total_bonus = 0
+max_bonus = 0
+max_attendance = 0
+
+
+for student in range(number_students):
+    attendance = int(input())
+    total_bonus = (attendance / number_lectures) * (5 + additional_bonus)
+    if total_bonus > max_bonus:
+        max_bonus = total_bonus
+        max_attendance = attendance
+
+print(f"Max Bonus: {ceil(max_bonus)}.")
+print(f"The student has attended {max_attendance} lectures.")
+
+
+
+
+
+
+
+
