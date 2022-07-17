2fbc1c6737cf452c57e5c8e0836141a446424a43
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-17 15:47:29 +0300

    Add files via upload

---
+++ mid_exams_first_task/sodtuni_reception_third.py
@@ -0,0 +1,19 @@
+
+first_employee_efficiency = int(input())
+second_employee_efficiency = int(input())
+third_employee_efficiency = int(input())
+students_count = int(input())
+
+all_efficiency_per_hour = first_employee_efficiency + second_employee_efficiency + third_employee_efficiency
+needed_hours = 0
+
+while students_count >= 0:
+    students_count -= all_efficiency_per_hour
+    needed_hours += 1
+
+    if needed_hours % 4 == 0:
+        needed_hours += 1
+
+
+print(f"Time needed: {needed_hours}h.")
+
