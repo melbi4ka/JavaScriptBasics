9552b7e739f3321523cb1dfa2d23097986068182
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-21 16:24:45 +0300

    Add files via upload

---
+++ mid_exam_second_task/the_lift_third.py
@@ -0,0 +1,35 @@
+all_people = int(input())
+current_state = list(map(int, input().split()))
+
+
+for k in range(len(current_state)):
+    if all_people == 0:
+        break
+
+    if current_state[k] == 0:
+        if all_people >= 4:
+            all_people -= 4
+            current_state[k] += 4
+        elif all_people < 4:
+            current_state[k] += all_people
+            all_people = 0
+    elif 0 < current_state[k] <= 4:
+        if all_people >= 4:
+            all_people -= 4 - current_state[k]
+            current_state[k] = 4
+        else:
+            current_state[k] += all_people
+            all_people = 0
+
+
+counter = current_state.count(4)
+if all_people == 0 and counter == len(current_state):
+    print(" ".join(str(x) for x in current_state))
+
+elif all_people == 0 and counter < len(current_state):
+    print(f"The lift has empty spots!")
+    print(f"{' '.join(str(x) for x in current_state)}")
+
+elif all_people > 0 and counter == len(current_state):
+    print(f"There isn't enough space! {all_people} people in a queue!\n"
+          f"{' '.join(str(x) for x in current_state)}")
