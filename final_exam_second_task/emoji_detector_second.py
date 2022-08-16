ee02909ef71c89af44a02e4276624cab94100fef
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-08-16 20:51:41 +0300

    Update emoji_detector_second.py

--- final_exam_second_task/emoji_detector_second.py
+++ final_exam_second_task/emoji_detector_second.py
@@ -1,36 +1,3 @@
-# import re
-# import math
-#
-#
-# pattern = r"(::|\*\*)([A-Z][a-z]{2,})\1"
-#
-# text = input()
-#
-# numbers_as_list = []
-# for char in text:
-#     if char.isdigit():
-#         numbers_as_list.append(int(char))
-#
-# result = math.prod(numbers_as_list)
-#
-# matches = re.finditer(pattern,text)
-# cooler = []
-# counter = 0
-# for match in matches:
-#     word = match.group(2)
-#     coolnes = 0
-#     for char in word:
-#         coolnes += ord(char)
-#     if coolnes > result:
-#         cooler.append(match.group())
-#     counter += 1
-#
-# print(f"Cool threshold: {result}")
-# print(f"{counter} emojis found in the text. The cool ones are:")
-# print("\n".join(cooler))
-
-# в джъдж ми дава 0/100 защото дава, че прод не може да се импортне от библиотеката
-# прод е от версия 3.8 нагоре, а тяхната била 3.6
 
 import re
 from functools import reduce
@@ -63,5 +30,3 @@ for match in matches:
 print(f"Cool threshold: {result}")
 print(f"{counter} emojis found in the text. The cool ones are:")
 print("\n".join(cooler))
-
-
