afd69f2646e2cd3f561a8f5c3721683143c1787c
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-08-19 19:52:44 +0300

    Add files via upload

---
+++ final_exam_second_task/mirror words_second.py
@@ -0,0 +1,44 @@
+import re
+
+pattern = r"(@|#)([A-Za-z]{3,})\1\1([A-Za-z]{3,})\1"
+text = input()
+
+matches = re.finditer(pattern, text)
+end_list = []
+counter = 0
+
+for match in matches:
+    if match:
+        first_word = match.group(2)
+        second_word = match.group(3)
+        if first_word == second_word[::-1]:
+            mirrors = f"{first_word} <=> {second_word}"
+            end_list.append(mirrors)
+    counter += 1
+
+
+if len(end_list) == 0 and counter == 0:
+    print(f"No word pairs found!")
+    print(f"No mirror words!")
+else:
+    print(f"{counter} word pairs found!")
+    if len(end_list) == 0:
+        print(f"No mirror words!")
+    else:
+        print(f"The mirror words are:")
+        print(", ".join(end_list))
+
+# print(matches)
+# патърна - (@|#) започва или с кльомба или с хаштаг първа група
+# комбинация от главни и малки букви от 3 нагоре ([A-Za-z]{3,})
+# завършва с това, което е мачнато в първа група
+# продължава с това, което е мачнато в първа група
+# комбинация от главни и малки букви от 3 нагоре ([A-Za-z]{3,})
+# завършва с това, което е мачнато в първа група
+
+
+
+
+
+
+
