3ddb80f66b52e200d4e15cea53974d1cfc4c7dc3
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-10-04 20:33:25 +0300

    Add files via upload

---
+++ final_exam_second_task/vtora_zadacha_destination_mapper.py
@@ -0,0 +1,19 @@
+import re
+
+pattern = r"(=|\/)([A-Z][A-Za-z]{2,})\1"
+text = input()
+matches = re.finditer(pattern, text)
+cities_list = []
+
+for match in matches:
+    if match:
+        cities_list.append(match.group(2))
+
+travel_points = sum([len(el) for el in cities_list])
+
+print(f"Destinations: {', '.join(cities_list)}")
+print(f"Travel Points: {travel_points}")
+
+# print(cities_list)
+
+
