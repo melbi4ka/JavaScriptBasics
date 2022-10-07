4812d4dcd397dcef3ae4a4bdad71dff27dfb148f
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-10-07 21:56:05 +0300

    Add files via upload

---
+++ final_exam_second_task/vtora_zadacha_fancy_barcodes.py
@@ -0,0 +1,25 @@
+import re
+
+pattern = r"@#+([A-Z][A-Za-z0-9]{4,}[A-Z])@#+"
+
+number = int(input())
+for _ in range(number):
+    text = input()
+    match = re.search(pattern, text)
+    if match:
+        barcode = match.group(1)
+        barcode_group = ""
+        for char in barcode:
+            if char.isdigit():
+                barcode_group += char
+        if barcode_group == "":
+            barcode_group = "00"
+        print(f"Product group: {barcode_group}")
+
+    else:
+        print("Invalid barcode")
+
+
+
+
+    # print(match.group(1))
