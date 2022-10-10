babe8670779b944135ad5bf7703edbf27fd09327
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-10-10 09:28:05 +0300

    Add files via upload

---
+++ final_exam_second_task/vtora_zadacha_emoji_detector.py
@@ -0,0 +1,37 @@
+import re
+
+text = input()
+
+pattern = r"(\*\*|::)([A-Z][a-z]{2,})\1"
+num_pattern = r"\d"
+
+cools = list(map(int, re.findall(num_pattern, text)))
+
+cool_treshold = 1
+for n in cools:
+    cool_treshold = cool_treshold * n
+print(f"Cool threshold: {cool_treshold}")
+
+matches = re.finditer(pattern, text)
+cool_emoji = []
+count = 0
+
+for match in matches:
+    if match:
+        count += 1
+        emoji = match.group(2)
+        ascii_sum = 0
+        for char in emoji:
+            ascii_sum += ord(char)
+
+        if ascii_sum > cool_treshold:
+            cool_emoji.append(match.group())
+print(f"{count} emojis found in the text. The cool ones are:")
+x = "\n"
+print(f"{x.join(cool_emoji)}")
+
+
+
+
+
+
