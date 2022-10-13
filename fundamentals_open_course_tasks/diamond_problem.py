26969da687ac9b8dca23f75203f065a00111e84c
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-10-13 18:23:16 +0300

    Add files via upload

---
+++ fundamentals_open_course_tasks/diamond_problem.py
@@ -0,0 +1,32 @@
+import re
+
+text = input()
+pattern = r"(<(.+?)>)"
+
+matches = re.finditer(pattern, text)
+num_pattern = r"\d"
+no_match = True
+
+
+for match in matches:
+    if match:
+
+        word = match.group(2)
+
+        numbers = re.findall(num_pattern, word)
+
+        carats = sum([int(n) for n in numbers])
+        print(f"Found {carats} carat diamond")
+
+        no_match = False
+
+
+if no_match:
+    print("Better luck next time")
+
+
+
+        # print(carats)
+        # print(word)
+        # print(numbers)
+
