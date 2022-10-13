cb5a81351d9f870a511237e479d7cf9303b0788f
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-10-13 18:22:59 +0300

    Add files via upload

---
+++ fundamentals_open_course_tasks/replace_a_tag.py
@@ -0,0 +1,27 @@
+import re
+
+pattern = r"(.*?<a ?href=\".+\">.+<\/a>.*)"
+text = input()
+
+while text != "end":
+    matches = re.findall(pattern, text)
+    # print(matches)
+    if matches:
+        match = "".join(matches)
+        # print(match)
+
+        if "<a " in match:
+            match = match.replace("<a","[URL")
+        elif "<a" in match:
+            match = match.replace("<a", "[URL ")
+        if "\">" in match:
+            match = match.replace("\">", "\"]")
+        if "</a>" in match:
+            match = match.replace("</a>", "[/URL]")
+
+        print(match)
+
+    else:
+        print(text)
+
+    text = input()
